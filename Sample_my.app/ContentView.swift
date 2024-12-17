6ee548b9e8fc9f5c5b3cb29c2a0685cadaca7d78
mdigix <grouses_joiner_0i@icloud.com>
2024-12-17 09:18:58 +0900

    Sample_my.app source code (sample)

--- Sample_my.app/ContentView.swift
+++ Sample_my.app/ContentView.swift
@@ -8,17 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    //　状態変数の追加
+    @State private var message: String = "Hello, world!"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            //　テキストの表示
+            Text(message)
+                .font(.largeTitle)
+                .padding()
+            
+            //　ボタンを追加
+            Button(action: {
+                //　ボタンを押した時にメッセージを変更
+                message = "Happy SwiftUI!"
+            }) {
+                VStack(spacing: 20) {
+                    Text("Press star icon")
+                        .padding(.bottom, -12.0)
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                        .scaleEffect(0.9)
+                }
+                
+            }
+            
         }
-        .padding()
+        
     }
-}
-
-#Preview {
-    ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View{
+            ContentView()
+        }
+    }
 }
